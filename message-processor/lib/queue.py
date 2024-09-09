851c0846373825b18ee1563531a352c34b8977df
Juni Samos <jsamos@gmail.com>
2024-09-09 17:38:20 +0200

    Redis ENV

--- message-processor/lib/queue.py
+++ message-processor/lib/queue.py
@@ -1,5 +1,6 @@
 from redis import Redis
 from rq import Queue
+import os
 
-redis_conn = Redis(host='redis', port=6379)
+redis_conn = Redis(host=os.environ.get('REDIS_HOST'), port=os.environ.get('REDIS_PORT'))
 q = Queue('podcast_queue', connection=redis_conn)
