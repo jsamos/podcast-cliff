f4c6aca53f56b04abab12d924ec4c8c85cf69ec3
Juni Samos <jsamos@gmail.com>
2024-09-09 21:25:10 +0200

    Productionize

---
+++ api/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+if [ "$FLASK_ENV" = "production" ]; then
+    echo "Running in production mode"
+    exec gunicorn --bind 0.0.0.0:5000 app:app
+else
+    echo "Running in development mode"
+    exec python -m flask run --host=0.0.0.0
+fi
