80165e6eb105cca0a662a9171c8c9eec8f939f52
Juni Samos <jsamos@gmail.com>
2024-09-10 10:34:22 +0200

    Unicorn Workers In Production

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -2,7 +2,7 @@
 
 if [ "$FLASK_ENV" = "production" ]; then
     echo "Running in production mode"
-    exec gunicorn --bind 0.0.0.0:5000 app:app
+    exec gunicorn --bind 0.0.0.0:5000 --workers 4 app:app
 else
     echo "Running in development mode"
     exec python -m flask run --host=0.0.0.0
